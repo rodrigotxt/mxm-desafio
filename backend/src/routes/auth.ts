ca8abf924400872ed717d1d03207ef2adb94e860
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-13 19:52:02 -0300

    atualiza .env e auth route

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,10 @@
+// src/routes/auth.ts
+import { Router } from 'express';
+import { registerUser, loginUser } from '../controllers/authController';
+
+const router = Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+export default router;

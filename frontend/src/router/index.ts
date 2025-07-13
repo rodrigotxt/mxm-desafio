3d4bd0c4e2fee134b5aefd4e2fb3a473fd7402c4
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-13 18:38:13 -0300

    adiciona rotas iniciais

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth'
+
+// Importa os componentes das views
+import HomeView from '@/views/HomeView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +15,33 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/login',
+      name: 'login',
+      component: LoginView,
     },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardView,
+      meta: { requiresAuth: true },
+    },
+    // TODO: Adicionar outras rotas ...
   ],
 })
 
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore() // Acessa o store de autenticação
+  const requiresAuth = to.meta.requiresAuth // Verifica se a rota precisa de autenticacao
+
+  // Se a rota precisa autenticacao e o usuario nao esta autenticado
+  if (requiresAuth && !authStore.isAuthenticated) {
+    // Redireciona para a página de login
+    next('/login')
+  } else {
+    // Continua navegacao
+    next()
+  }
+})
+
 export default router
+

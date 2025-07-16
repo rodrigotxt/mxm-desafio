e21fdca00504510adbfbe9aa0d553cec23c2e114
Rodrigo Martins <rodrigo.martins@grupoivory.com.br>
2025-07-15 21:10:57 -0300

    test cors

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 
 const corsOptions = {
-  origin: 'http://localhost:5176',
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204
@@ -14,5 +14,6 @@ export const corsMiddleware = cors(corsOptions);
 // Middleware de autenticação
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // TODO: implementar autenticação
+  console.log('Autenticação bem-sucedida!');
   next();
 };
